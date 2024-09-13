95f160a3ae83979de99a2b4e95a7b8de0384a351
Tomas Macchi <tomas@mutual.app>
2024-09-13 19:54:05 -0300

    fix secrets

--- src/lib/oauth2-client.ts
+++ src/lib/oauth2-client.ts
@@ -1,9 +1,11 @@
 import { google } from 'googleapis'
-const CLIENT_ID = '768524797731-elm1o7rl7g0m0upg23qbk9dp1tok3eqk.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'http://localhost:3000/api/oauth2callback';
 
-export const oauth2Client = new google.auth.OAuth2(
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID!;
+const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET!;
+const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI!;
+
+// Initialize the OAuth2 client
+const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
     CLIENT_SECRET,
     REDIRECT_URI
